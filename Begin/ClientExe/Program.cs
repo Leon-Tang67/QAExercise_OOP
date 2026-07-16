c38c2e5f87fe2a3f457872dc7181f25db895cd2f
Liang <Leon.tang67@gmail.com>
2026-07-16 09:10:27 +0100

    Initial commit: copied intial code into the repo to begin with

---
+++ Begin/ClientExe/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ClientExe {
+  class Program {
+    static void Main(string[] args) {
+
+    }
+  }
+}
